a20ad35a40bcaf39702deee2dc8bc67dcd6afd36
Jesus Clemente <jcdeveloper92@gmail.com>
2023-08-26 01:35:57 -0400

    enabled the ssl mode in sequelize

--- src/utils/database.js
+++ src/utils/database.js
@@ -7,6 +7,12 @@ const database = new Sequelize({
   username: config.db.user,
   password: config.db.password,
   database: config.db.dbName,
+  ssl: process.env.DB_ENABLE_SSL,
+  dialectOptions: {
+    ssl: process.env.DB_ENABLE_SSL && {
+      require: true
+    }
+  }
 });
 
 module.exports = database;
