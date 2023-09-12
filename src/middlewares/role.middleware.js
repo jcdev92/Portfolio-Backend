e3b73a76f7d60cfbb5e959c8a3d3fb171f5d5293
Jesus Clemente <jcdeveloper92@gmail.com>
2023-09-12 16:03:53 -0400

    response 401 message amplified during test - 3

--- src/middlewares/role.middleware.js
+++ src/middlewares/role.middleware.js
@@ -1,6 +1,6 @@
 const adminValidate = (req, res, next) => {
   if (req.user.role !== 'admin') {
-      return res.status(401).json({ message: 'Unauthorized' });
+      return res.status(401).json({ message: 'Unauthorized, you dont have admin permissions' });
   }
   next();
 }
